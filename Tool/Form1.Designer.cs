bec84ba904d66bfdd806f531298a27c1599bda45
Tran Tuan Minh <ttminhblue@gmail.com>
2025-07-05 16:36:07 +0700

    Initial commit

---
+++ Tool/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUrl = new TextBox();
+            btnClick = new Button();
+            label1 = new Label();
+            txtContent = new TextBox();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            txtToken = new TextBox();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Location = new Point(72, 26);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(313, 23);
+            txtUrl.TabIndex = 0;
+            // 
+            // btnClick
+            // 
+            btnClick.Location = new Point(1035, 25);
+            btnClick.Name = "btnClick";
+            btnClick.Size = new Size(75, 23);
+            btnClick.TabIndex = 1;
+            btnClick.Text = "Click";
+            btnClick.UseVisualStyleBackColor = true;
+            btnClick.Click += btnClick_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 2;
+            label1.Text = "URL:";
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(23, 62);
+            txtContent.Multiline = true;
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(1087, 489);
+            txtContent.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(450, 27);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(234, 23);
+            txtEmail.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(405, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(699, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Token:";
+            // 
+            // txtToken
+            // 
+            txtToken.Location = new Point(746, 26);
+            txtToken.Name = "txtToken";
+            txtToken.Size = new Size(283, 23);
+            txtToken.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1136, 563);
+            Controls.Add(txtToken);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtEmail);
+            Controls.Add(txtContent);
+            Controls.Add(label1);
+            Controls.Add(btnClick);
+            Controls.Add(txtUrl);
+            Name = "Form1";
+            Text = "Generate Variants";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUrl;
+        private Button btnClick;
+        private Label label1;
+        private TextBox txtContent;
+        private TextBox txtEmail;
+        private Label label2;
+        private Label label3;
+        private TextBox txtToken;
+    }
+}
---
+++ Tool/Form1.cs
@@ -0,0 +1,350 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Tool
+{
+    public partial class Form1 : Form
+    {
+        private DataSourceInfo datasource = new DataSourceInfo();
+        public class DataSourceInfo
+        {
+            public string DatasourceName { get; set; }
+            public string DatasourceGroupName { get; set; }
+            public string CommandName { get; set; }
+            public string DatasourceID { get; set; }
+            public string Countries { get; set; }
+            public List<VariantModel> ListVariants { get; set; }
+        }
+        public class VariantModel
+        {
+            public string VariantName { get; set; }
+            public string NameFormat { get; set; }
+            public string AddressFormat { get; set; }
+            public string Note { get; set; }
+            public bool Priority { get; set; }
+            public List<FieldInfo> Fields { get; set; }
+        }
+        public class FieldInfo
+        {
+            public string FieldName { get; set; }
+            public bool RequiredInput { get; set; }
+            public bool OptionalInput { get; set; }
+            public bool Output { get; set; }
+            public bool Appended { get; set; }
+            public string Comments { get; set; }
+        }
+        private bool convertStringToBool(string value)
+        {
+            if (value.ToLower().Equals("yes", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+        private string GenerateDatasourceClass(DataSourceInfo ds)
+        {
+            var sb = new StringBuilder();
+
+            string className = ds.CommandName?.Replace(" ", "") ?? "GeneratedClass";
+
+            sb.AppendLine($"public class {className} : CreateDatasourceRequest");
+            sb.AppendLine("{");
+            sb.AppendLine($"    private const int _datasourceID        = DatasourceIds.{className};");
+            sb.AppendLine($"    private const int _commandID          = CommandTypeIds.{className};");
+            sb.AppendLine($"    private const string _datasourceGroupName = \"{ds.DatasourceGroupName}\";");
+            sb.AppendLine($"    private const string _datasourceName      = \"{ds.DatasourceName}\";");
+            sb.AppendLine($"    private const string _commandName         = \"{ds.CommandName}\";");
+            sb.AppendLine($"    private static readonly int[] _countryIDs = [(int)CountryEnum.{ds.Countries}];");
+            sb.AppendLine();
+
+            int variantIndex = 1;
+            foreach (var variant in ds.ListVariants)
+            {
+                // comment theo note
+                string noteComment = !string.IsNullOrWhiteSpace(variant.Note)
+                    ? $"// Variant {variantIndex}: {variant.Note}"
+                    : $"// Variant {variantIndex}";
+
+                sb.AppendLine($"    {noteComment}");
+                sb.AppendLine($"    private readonly List<(int fieldId, bool isRequired, bool isOptional, bool isOutput, bool isAppended)> _fieldsVariant{variantIndex} =");
+                sb.AppendLine("    [");
+
+                // Tìm độ dài field name dài nhất
+                int maxFieldNameLen = variant.Fields.Max(f => f.FieldName.Length);
+
+                foreach (var field in variant.Fields)
+                {
+                    string fieldName = field.FieldName.PadRight(maxFieldNameLen);
+                    string required = field.RequiredInput ? "true " : "false";
+                    string optional = field.OptionalInput ? "true " : "false";
+                    string output = field.Output ? "true " : "false";
+                    string appended = field.Appended ? "true " : "false";
+
+                    string comment = !string.IsNullOrWhiteSpace(field.Comments) ? $" // {field.Comments}" : "";
+
+                    sb.AppendLine(
+                        $"        ((int)FieldEnum.{fieldName}, {required}, {optional}, {output}, {appended}), {comment}"
+                    );
+                }
+
+                sb.AppendLine("    ];");
+                sb.AppendLine();
+                variantIndex++;
+            }
+
+            sb.AppendLine($"    public {className}()");
+            sb.AppendLine("    {");
+            sb.AppendLine("        IAppSettingsHelper appSettingsHelper = Gateway.Common.ObjectFactory.GetInstance<IAppSettingsHelper>();");
+            sb.AppendLine("        Datasource = new DatasourceIdentificationData");
+            sb.AppendLine("        {");
+            sb.AppendLine($"            Credential = new CredentialData");
+            sb.AppendLine("            {");
+            sb.AppendLine($"                UserName        = appSettingsHelper.GetString(\"{className}.Username\"),");
+            sb.AppendLine($"                TestUserName   = appSettingsHelper.GetString(\"{className}.TestUsername\"),");
+            sb.AppendLine($"                EndpointUrl    = appSettingsHelper.GetString(\"{className}.EndpointUrl\"),");
+            sb.AppendLine($"                TestEndpointUrl= appSettingsHelper.GetString(\"{className}.TestEndpointUrl\"),");
+            sb.AppendLine($"                Password       = appSettingsHelper.GetString(\"{className}.Password\"),");
+            sb.AppendLine($"                TestPassword   = appSettingsHelper.GetString(\"{className}.TestPassword\"),");
+            sb.AppendLine($"                CredentialFormat = string.Empty");
+            sb.AppendLine("            },");
+            sb.AppendLine("            DatasourceId    = _datasourceID,");
+            sb.AppendLine("            CommandId       = _commandID,");
+            sb.AppendLine("            DatasourceName  = _datasourceName,");
+            sb.AppendLine("            CommandName     = _commandName,");
+            sb.AppendLine("            Industries      = Enum.GetValues(typeof(IndustryEnum)).Cast<IndustryEnum>().Select(x => (int)x).ToArray(),");
+            sb.AppendLine("            ProductList     = [(int)ProductEnum.IdentityVerification],");
+            sb.AppendLine("            IsTestable      = true,");
+            sb.AppendLine("            AllowAppendData = true,");
+            sb.AppendLine($"            CountryFields   = new Dictionary<int, int[]> {{ {{ (int)CountryEnum.{ds.Countries}, [] }} }}");
+            sb.AppendLine("        };");
+            sb.AppendLine();
+
+            sb.AppendLine("        DatasourceGroupVariants =");
+            sb.AppendLine("        [");
+
+            for (int i = 0; i < ds.ListVariants.Count; i++)
+            {
+                var variant = ds.ListVariants[i];
+                var fieldVar = $"_fieldsVariant{i + 1}";
+                string addressFormat = variant.AddressFormat?.ToLower().Contains("address1") == true ? "Address1" : "Segmented";
+                string nameFormat = variant.NameFormat?.ToLower().Contains("segmented") == true ? "Segmented" : "Segmented";
+                string note = variant.Note?.Replace("\"", "\\\"") ?? "";
+
+                sb.AppendLine("            new DynamicDatasourceGroupVariantData");
+                sb.AppendLine("            {");
+                sb.AppendLine("                DatasourceGroupName = _datasourceGroupName,");
+                sb.AppendLine("                DatasourceId        = _datasourceID,");
+                sb.AppendLine($"                Fields              = {fieldVar}.Select(x => x.fieldId).ToArray(),");
+                sb.AppendLine("                CountryIds          = _countryIDs,");
+                sb.AppendLine($"                RequiredFields      = [..{fieldVar}.Where(f => f.isRequired).Select(x => x.fieldId)],");
+                sb.AppendLine($"                OptionalFields      = [..{fieldVar}.Where(f => f.isOptional).Select(x => x.fieldId)],");
+                sb.AppendLine($"                OutputFields        = [..{fieldVar}.Where(f => f.isOutput).Select(x => x.fieldId)],");
+                sb.AppendLine($"                AppendedFields      = [..{fieldVar}.Where(f => f.isAppended).Select(x => x.fieldId)],");
+                sb.AppendLine("                SourceType          = _datasourceGroupName,");
+                string parsedPriority = variant.Priority ? "true" : "false";
+                sb.AppendLine($"                Priority            = {parsedPriority},");
+                sb.AppendLine($"                AddressFormat       = (int)AddressFormatEnum.{addressFormat},");
+                sb.AppendLine($"                NameFormat          = (int)NameFormatEnum.{nameFormat},");
+                sb.AppendLine($"                Notes               = \"{note}\"");
+                sb.Append(i == ds.ListVariants.Count - 1 ? "            }" : "            },");
+            }
+
+            sb.AppendLine("        ];");
+            sb.AppendLine("    }");
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        private DataSourceInfo ParseDatasourceInfo(HtmlAgilityPack.HtmlDocument doc)
+        {
+            var info = new DataSourceInfo();
+
+            var table = doc.DocumentNode.SelectSingleNode("//table[.//th[contains(text(), 'Vendor Name')]]");
+
+            if (table == null)
+                return info;
+
+            var rows = table.SelectNodes(".//tr");
+            if (rows == null)
+                return info;
+
+            foreach (var row in rows)
+            {
+                var th = row.SelectSingleNode(".//th");
+                var td = row.SelectSingleNode(".//td");
+
+                if (th == null || td == null) continue;
+
+                string key = HtmlEntity.DeEntitize(th.InnerText.Trim());
+                string value = HtmlEntity.DeEntitize(td.InnerText.Trim());
+
+                switch (key.ToLowerInvariant())
+                {
+                    case var s when s.Contains("internal datasource name"):
+                        info.DatasourceName = value;
+                        break;
+
+                    case var s when s.Contains("public datasource group name"):
+                        info.DatasourceGroupName = value;
+                        break;
+
+                    case "command name":
+                        info.CommandName = value;
+                        break;
+
+                    case "datasource id":
+                        info.DatasourceID = value;
+                        break;
+
+                    case "countries":
+                        info.Countries = value;
+                        break;
+                }
+            }
+
+            return info;
+        }
+
+        private List<VariantModel> GetVariants(HtmlAgilityPack.HtmlDocument doc)
+        {
+            var variants = new List<VariantModel>();
+            var variantNodes = doc.DocumentNode.SelectNodes("//*[contains(text(), 'Variant #')]");
+
+            if (variantNodes == null) return variants;
+
+            foreach (var variantNode in variantNodes)
+            {
+                var variant = new VariantModel();
+                variant.VariantName = variantNode.InnerText.Trim();
+                if (!variant.VariantName.StartsWith("Variant"))
+                    continue;
+
+                HtmlNode? metaTable = doc.DocumentNode
+                    .SelectNodes("//table[.//th[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'), 'informationtype')]]")
+                    ?.OrderBy(t => Math.Abs(t.StreamPosition - variantNode.StreamPosition))
+                    .FirstOrDefault();
+
+                ParseMetadata(metaTable, variant);
+
+                var fieldTables = doc.DocumentNode
+                    .SelectNodes("//table[.//th[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'), 'fieldname')]]");
+
+                var table = fieldTables?
+                    .Where(t => t.StreamPosition > variantNode.StreamPosition)
+                    .OrderBy(t => t.StreamPosition)
+                    .FirstOrDefault();
+
+                variant.Fields = ParseFieldTable(table);
+
+                variants.Add(variant);
+            }
+            return variants;
+        }
+
+        private void ParseMetadata(HtmlNode metaTable, VariantModel variant)
+        {
+            if (metaTable == null) return;
+
+            var rows = metaTable.SelectNodes(".//tr");
+            if (rows == null) return;
+
+            foreach (var row in rows.Skip(1))
+            {
+                var cells = row.SelectNodes("td");
+                if (cells == null || cells.Count < 2) continue;
+
+                string key = HtmlEntity.DeEntitize(cells[0].InnerText.Trim()).ToLowerInvariant();
+                string value = HtmlEntity.DeEntitize(cells[1].InnerText.Trim());
+
+                switch (key)
+                {
+                    case "name format":
+                        variant.NameFormat = value;
+                        break;
+                    case "address format":
+                        variant.AddressFormat = value;
+                        break;
+                    case "product type":
+                        break;
+                    case "note":
+                        variant.Note = value;
+                        break;
+                    case "priority":
+                        variant.Priority = convertStringToBool(value);
+                        break;
+                }
+            }
+        }
+
+        private List<FieldInfo> ParseFieldTable(HtmlNode? table)
+        {
+            var fields = new List<FieldInfo>();
+
+            if (table == null)
+                return fields;
+
+            var rows = table.SelectNodes(".//tr");
+            if (rows == null || rows.Count <= 1)
+                return fields;
+
+            foreach (var row in rows.Skip(1))
+            {
+                var cells = row.SelectNodes("td|th");
+                if (cells == null || cells.Count < 2) continue;
+
+                string GetCell(int index)
+                {
+                    return index < cells.Count ? HtmlEntity.DeEntitize(cells[index].InnerText.Trim()) : string.Empty;
+                }
+
+                var field = new FieldInfo
+                {
+                    FieldName = GetCell(1),
+                    RequiredInput = convertStringToBool(GetCell(2)),
+                    OptionalInput = convertStringToBool(GetCell(3)),
+                    Output = convertStringToBool(GetCell(4)),
+                    Appended = convertStringToBool(GetCell(5)),
+                    Comments = GetCell(6)
+                };
+
+                fields.Add(field);
+            }
+
+            return fields;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtContent.Font = new Font("Consolas", 10);
+        }
+
+        private async void btnClick_Click(object sender, EventArgs e)
+        {
+            string confluenceUrl = txtUrl.Text;
+            string username = txtEmail.Text;
+            string apiToken = txtToken.Text;
+
+            string basicAuth = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{username}:{apiToken}"));
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuth);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(confluenceUrl);
+                var content = await response.Content.ReadAsStringAsync();
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(content);
+                datasource = ParseDatasourceInfo(doc);
+                var listVariants = GetVariants(doc);
+                datasource.ListVariants = listVariants;
+
+                string text = GenerateDatasourceClass(datasource);
+                txtContent.Text = text;
+            }
+        }
+    }
+}
